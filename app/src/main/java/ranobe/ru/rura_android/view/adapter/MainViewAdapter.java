c0a9d7c77d4c469743b12c00b384fffe60884159
Andrey Sviridov <andrey.sviridov4@gmail.com>
2017-03-24 19:06:25 +0300

    added picasso (upload banners)

--- app/src/main/java/ranobe/ru/rura_android/view/adapter/MainViewAdapter.java
+++ app/src/main/java/ranobe/ru/rura_android/view/adapter/MainViewAdapter.java
@@ -5,7 +5,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.List;
 import ranobe.ru.rura_android.R;
@@ -16,7 +18,6 @@ public class MainViewAdapter extends RecyclerView.Adapter<MainViewAdapter.ViewHo
 
   private List<Preview> previews = new ArrayList<>();
   private static final String TAG = "RecyclerViewAdapter";
-
 
   public void setProjectDTOs(List<Preview> previews) {
     this.previews = previews;
@@ -33,6 +34,8 @@ public class MainViewAdapter extends RecyclerView.Adapter<MainViewAdapter.ViewHo
     Preview preview = previews.get(position);
     holder.name.setText(preview.getProjectName());
     holder.author.setText(preview.getAuthor());
+    Picasso.with(holder.banner.getContext()).load("http:" + preview.getUrlBanner()).resize(400, 125)
+        .into(holder.banner);
     holder.itemView.setOnClickListener(view -> view.getContext().startActivity(new Intent(view.getContext(), ProjectActivity.class)));
   }
 
@@ -43,11 +46,13 @@ public class MainViewAdapter extends RecyclerView.Adapter<MainViewAdapter.ViewHo
   class ViewHolder extends RecyclerView.ViewHolder {
     private TextView name;
     private TextView author;
+    private ImageView banner;
 
     ViewHolder(View itemView) {
       super(itemView);
       name = (TextView) itemView.findViewById(R.id.projectName);
       author = (TextView) itemView.findViewById(R.id.authorName);
+      banner = (ImageView) itemView.findViewById(R.id.banner);
     }
   }
 }

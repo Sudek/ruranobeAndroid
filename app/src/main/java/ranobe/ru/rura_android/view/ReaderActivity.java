63841108c708742f1fa86567a54cf15e876d2cac
Andrey Sviridov <andrey.sviridov4@gmail.com>
2017-04-20 22:39:29 +0300

    WIP reader screen

--- app/src/main/java/ranobe/ru/rura_android/view/ReaderActivity.java
+++ app/src/main/java/ranobe/ru/rura_android/view/ReaderActivity.java
@@ -2,40 +2,56 @@ package ranobe.ru.rura_android.view;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 import butterknife.Bind;
-import butterknife.ButterKnife;
 import java.util.List;
 import ranobe.ru.rura_android.R;
-import ranobe.ru.rura_android.presenter.ReaderPresenter;
-import ranobe.ru.rura_android.presenter.ReaderPresenterImpl;
 import ranobe.ru.rura_android.presenter.entities.Text;
+import ranobe.ru.rura_android.view.adapter.ReaderPagerAdapter;
+import ranobe.ru.rura_android.view.fragment.ReaderFragment;
 
 public class ReaderActivity extends AppCompatActivity implements ReaderView {
 
   private final static String VOLUME_ID = "VOLUME_ID";
   private int volumeId;
+
   @Bind(R.id.web_view) WebView webView;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.text_layout);
-    ButterKnife.bind(this);
+    setContentView(R.layout.tab_layout);
 
     Bundle bundle = getIntent().getExtras();
     volumeId = bundle.getInt(VOLUME_ID);
 
-    ReaderPresenter readerPresenter = new ReaderPresenterImpl(this, volumeId);
-    readerPresenter.showText();
+    ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+    setupViewPager(viewPager);
+
+    //TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+    //tabLayout.setupWithViewPager(viewPager);
   }
 
   @Override public void showData(List<Text> text) {
-    webView.loadData(text.get(1).getTextHtml(), "text/html; charset=utf-8", "UTF-8");
+    //webView.loadData(text.get(1).getTextHtml(), "text/html; charset=utf-8", "UTF-8");
     //webView.loadUrl("http://ruranobe.ru/r/ddmmd/v1/p");
   }
 
   @Override public void showError(String error) {
 
+  }
+
+  private void setupViewPager(ViewPager viewPager) {
+    ReaderPagerAdapter adapter = new ReaderPagerAdapter(getSupportFragmentManager());
+    adapter.addFragment(new ReaderFragment(), "1");
+    adapter.addFragment(new ReaderFragment(), "2");
+    adapter.addFragment(new ReaderFragment(), "3");
+    viewPager.setCurrentItem(1);
+    viewPager.setAdapter(adapter);
+  }
+
+  public int getVolumeId() {
+    return volumeId;
   }
 }
--- app/src/main/java/ranobe/ru/rura_android/view/adapter/ReaderPagerAdapter.java
+++ app/src/main/java/ranobe/ru/rura_android/view/adapter/ReaderPagerAdapter.java
@@ -6,12 +6,12 @@ import android.support.v4.app.FragmentPagerAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProjectPagerAdapter extends FragmentPagerAdapter {
+public class ReaderPagerAdapter extends FragmentPagerAdapter {
   private final List<Fragment> mFragmentList = new ArrayList<>();
   private final List<String> mFragmentTitleList = new ArrayList<>();
 
-  public ProjectPagerAdapter(FragmentManager manager) {
-    super(manager);
+  public ReaderPagerAdapter(FragmentManager fm) {
+    super(fm);
   }
 
   public void addFragment(Fragment fragment, String title) {
@@ -19,18 +19,16 @@ public class ProjectPagerAdapter extends FragmentPagerAdapter {
     mFragmentTitleList.add(title);
   }
 
-  @Override
-  public Fragment getItem(int position) {
+  @Override public Fragment getItem(int position) {
     return mFragmentList.get(position);
   }
 
-  @Override
-  public int getCount() {
+  @Override public int getCount() {
     return mFragmentList.size();
   }
 
-  @Override
-  public CharSequence getPageTitle(int position) {
+  @Override public CharSequence getPageTitle(int position) {
     return mFragmentTitleList.get(position);
   }
+
 }
